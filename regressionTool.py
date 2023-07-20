bc8de3fbfdd26d1bf6ef7a9a7c093ddd9a0cee96
DDA <dustarm86@users.noreply.github.com>
2023-07-19 20:18:33 -0400

    Update regressionTool.py
    added some more comments to the function of parts of the code
    fixed formatting

--- regressionTool.py
+++ regressionTool.py
@@ -3,29 +3,29 @@ import time
 import json
 from commandsByIntent import commandsList
 
-# instructions: replace your "from X import commandsList" with the correct list of commands you want to send
+# instructions: replace your "from X import commandsList" with the correct list of commands you want to send from the commandsByIntent.py file
 
 start_time=time.time()
-# insert receiverId value of device you are testing
-DeviceId = 1234567890
-
+# insert deviceId and fakeAccountId value of device you are testing
+deviceId = 1234567890
+fakeAccountId = 999888777666
+# builds the cURL with the endpoint/url, satToken, and other needed parameters for the request
 def speak_command(command):
     url = "http://fakeProductionOrLowerEnvEndpoint:1234/fakeAppId=STB&fakeFiltersAndOtherCurlParameters&Language=eng&DeviceId={}&partnerid=fakePartner".format(DeviceId)
          
-     
-#  "Authorization" parameter below must be updated with a newly generated SAT token every 24 hours
+    #  "Authorization" parameter below must be updated with a newly generated SAT token every 24 hours
     headers = {
         'Content-Type': "text/plain",
         'Authorization': "Bearer <satToken>"
         }
     data = command
     response = requests.request("POST", url, headers=headers, data=data)
-
-    #print(response.text)
     print(response.json())
     return response
-    #json.load(response.text)
 
+# this loop below  pulls the list of commands named commandsList within the file CommandsByIntent.py and works its way down from the top of the list of commands to the bottom. 
+# once it gets to the last command in the list the program will stop. you can modify this list to replace it with any command you'd like to send. 
+# this is how you build group of commands for test cases in your test coverage (e.g. smoke, sanity, regression, top 100, etc.)
 stuff_to_yell_at_remote = commandsList
 for command in stuff_to_yell_at_remote:
     speak_command("exit")
